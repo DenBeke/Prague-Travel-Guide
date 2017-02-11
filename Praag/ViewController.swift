34258fecaf26405e51310efa7501f24b4a61f2e4
MathiasB <git@denbeke.be>
2017-02-11 17:49:18 +0100

    Cleanup and error handling

--- Praag/ViewController.swift
+++ Praag/ViewController.swift
@@ -20,32 +20,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        pageTitle.title = inputPage.name
-        
-        //webView = WKWebView(frame: view.bounds)
-        //view.addSubview(webView!)
-        //webView?.load(URLRequest(url: pageUrl!))
-        
-        let content = try? String(contentsOfFile: Bundle.main.path(forResource: "pages/\(inputPage.url)", ofType: "md")!)
-        let down = Down(markdownString: content!)
-        let html = try? down.toHTML()
-        let base = Bundle.main.bundleURL.appendingPathComponent("pages")
-        
-        webView = WKWebView(frame: self.view.bounds)
-        //webView?.loadHTMLString(html!, baseURL: base)
 
-        var template = try? String(contentsOf: base.appendingPathComponent("index.html"))
-        template = template?.replacingOccurrences(of: "DOWN_HTML", with: html!)
-        _ = webView?.loadHTMLString(template!, baseURL: base)
+        do {
+            pageTitle.title = inputPage.name
+            
+            // Load markdown file
+            let content = try String(contentsOfFile: Bundle.main.path(forResource: "pages/\(inputPage.url)", ofType: "md")!)
+            
+            // Convert markdown to html
+            let down = Down(markdownString: content)
+            let html = try down.toHTML()
+            let base = Bundle.main.bundleURL.appendingPathComponent("pages")
+            
+            
+            // Create webview and add html to it
+            webView = WKWebView(frame: self.view.bounds)
+            var template = try String(contentsOf: base.appendingPathComponent("index.html"))
+            template = template.replacingOccurrences(of: "DOWN_HTML", with: html)
+            _ = webView?.loadHTMLString(template, baseURL: base)
 
+        } catch {
+            alert(title: "Ooops", message: "Couldn't load this page")
+        }
         
-        
-        print(Bundle.main.bundleURL.appendingPathComponent("pages"))
-        
-        //webView = try? DownView(frame: self.view.bounds, markdownString: content!)
         
         view.addSubview(webView!)
+        
+    }
+    
+    
+    func alert(title: String, message: String = "") {
+        
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        // Create default action
+        let defaultAction = UIAlertAction(title: "Close", style: .default, handler: { (action) in
+            _ = self.navigationController?.popToRootViewController(animated: true)
+        })
+        
+        // Add action
+        alertController.addAction(defaultAction)
+        
+        present(alertController, animated: true, completion: nil)
         
     }
     
@@ -54,8 +70,6 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    @IBOutlet weak var test: UINavigationItem!
 
 }
 
